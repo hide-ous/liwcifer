0986a8219c09f05ddaaa42c4b9c5d8b3cdeacba0
hide-ous <mattia.samory@gmail.com>
2023-05-31 12:06:21 +0200

    * common string substitutions

--- liwcifer/preprocess.py
+++ liwcifer/preprocess.py
@@ -2,6 +2,27 @@ import re
 import string
 
 ESCAPE_PUNCT_RE = re.compile('[%s]' % re.escape(string.punctuation))
+COMMON_REPLACEMENTS = [('w/', 'with'),
+                       ('b/', 'between'),
+                       ('&', 'and'),
+                       ("'cause", 'because'),
+                       ('and/or', 'and - or'),
+                       ("'an", 'and'),
+                       ("'n", 'and'),
+                       ('mos', 'months'),
+                       ('sec', 'second'),
+                       ("@", 'at')]
+
+# TODO:
+# Entry Type Entry Example Recommended Replacement:
+# E-mail Address your.name@example.com subEmailaddress
+# URL address http://www.LIWC.net subURLaddress
+# Hashtag #LIWCisawesome subHashtag
+# Twitter Handle @jwpennebaker subTwittername
+def substitutions(x):
+    for orig, repl in COMMON_REPLACEMENTS:
+        x=x.replace(orig, repl)
+    return x
 
 def remove_urls(x):
     return re.sub("http(.+)?(\W|$)", ' ', x)
@@ -20,8 +41,9 @@ def lower(x):
 
 
 def preprocess_pre_tokenizing(x):
-    return normalize_spaces(
-            remove_urls(x))
+    return substitutions(
+        normalize_spaces(
+            remove_urls(x)))
 
 
 def preprocess_e2e(x):
